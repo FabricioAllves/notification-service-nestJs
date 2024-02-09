beb46faefb3e56456c4049885d5157b5ccb3b5ce
FabricioAllves <fabricioohh@gmail.com>
2024-02-09 16:53:31 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { SMTPMailService } from './mail/smtp-email.service';
+import { MailService } from './mail/mail.service';
+import { HttpModule } from './http.module';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: MailService,
+      useClass: SMTPMailService,
+    },
+  ],
+})
+export class AppModule {}
